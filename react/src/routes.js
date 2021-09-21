c207384e1faac15d5af878dd85b49c7278d36740
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2021-09-20 22:57:48 -0700

    add routes file for frontend pages

---
+++ react/src/routes.js
@@ -0,0 +1,57 @@
+import HomePage from './home/pages/HomePage';
+import FeatureFlagPage from './admin/featureFlags/FeatureFlagPage';
+import CreateAccountPage from './accounts/create/CreateAccountPage';
+import LoginPage from './accounts/pages/LoginPage';
+import ScrabblePlayPage from './scrabble/pages/ScrabblePlayPage';
+import PazaakPlayPage from 'pazaak/PazaakPlayPage';
+import ChatroomPage from './chatroom/pages/ChatroomPage';
+import NotFoundPage from './dashboard/pages/NotFoundPage';
+
+const ROUTES = [
+    {
+        id: 'notFound',
+        path: '/not-found',
+        component: NotFoundPage,
+    },
+    {
+        id: 'createAccount',
+        path: '/accounts/create',
+        component: CreateAccountPage,
+    },
+    {
+        id: 'login',
+        path: '/accounts/login',
+        component: LoginPage,
+    },
+    {
+        id: 'home',
+        path: '/home',
+        component: HomePage,
+    },
+    {
+        id: 'featureFlags',
+        path: '/admin/feature-flags',
+        component: FeatureFlagPage,
+    },
+    {
+        id: 'chatroom',
+        path: '/chatroom',
+        component: ChatroomPage,
+    },
+    {
+        id: 'pazaakPlay',
+        path: '/pazaak/play',
+        component: PazaakPlayPage,
+    },
+    {
+        id: 'scrabblePlay',
+        path: '/scrabble/play',
+        component: ScrabblePlayPage,
+    },
+];
+
+function processRoutes() {
+    return ROUTES;
+}
+
+export default processRoutes();
