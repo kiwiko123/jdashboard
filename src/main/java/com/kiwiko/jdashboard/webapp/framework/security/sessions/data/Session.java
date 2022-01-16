af68eb8b2d8ae418d7097609e00e1725bcb5b22b
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2022-01-16 13:19:35 -0800

    delete deprecated DAO type

--- src/main/java/com/kiwiko/jdashboard/webapp/framework/security/sessions/data/Session.java
+++ src/main/java/com/kiwiko/jdashboard/webapp/framework/security/sessions/data/Session.java
@@ -1,18 +1,19 @@
 package com.kiwiko.jdashboard.webapp.framework.security.sessions.data;
 
-import com.kiwiko.library.persistence.dataAccess.data.AuditableDataEntityDTO;
-import com.kiwiko.jdashboard.webapp.users.data.User;
+import com.kiwiko.library.persistence.data.api.interfaces.SoftDeletableDataEntityDTO;
 import org.springframework.lang.Nullable;
 
 import java.time.Instant;
 import java.util.Optional;
 
-public class Session extends AuditableDataEntityDTO {
+public class Session extends SoftDeletableDataEntityDTO {
 
     private String token;
     private Instant startTime;
     private @Nullable Instant endTime;
     private @Nullable Long userId;
+    private Instant createdDate;
+    private Instant lastUpdatedDate;
 
     public String getToken() {
         return token;
@@ -45,5 +46,21 @@ public class Session extends AuditableDataEntityDTO {
 
     public void setUserId(@Nullable Long userId) {
         this.userId = userId;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(Instant lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
     }
 }
