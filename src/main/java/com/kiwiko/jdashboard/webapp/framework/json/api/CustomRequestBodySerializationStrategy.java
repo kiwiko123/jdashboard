e92a798b5e47d73a3efe74e01a65d8a8a59a1ad0
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2022-04-08 20:47:58 -0700

    delete legacy CustomRequestBody annotation

--- src/main/java/com/kiwiko/jdashboard/webapp/framework/json/api/CustomRequestBodySerializationStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.kiwiko.jdashboard.webapp.framework.json.api;
-
-import com.kiwiko.jdashboard.library.json.data.IntermediateJsonBody;
-
-public interface CustomRequestBodySerializationStrategy<T> {
-
-    T deserialize(IntermediateJsonBody body, T target);
-}
