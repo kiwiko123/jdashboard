bc1f5732a4e8a12420b5918034e2a71f1389268b
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2022-07-08 23:54:43 -0700

    working concept of swipe views

--- react/src/common/views/components/SwipeView.jsx
+++ react/src/common/views/components/SwipeView.jsx
@@ -1,20 +1,57 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import logger from 'tools/monitoring/logging';
 
 import './SwipeView.css';
 
 const DEFAULT_COORDINATES = { x: 0, y: 0 };
-const PULL_TO_VIEW_INTERACTIVE_PIXELS_DELTA = 50;
+const PULL_TO_VIEW_INTERACTIVE_PIXELS_HEIGHT = 50;
+const PULL_TO_VIEW_INTERACTIVE_PIXELS_WIDTH = 150;
 const CLICK_START_INTERACTIVE_PIXELS_THRESHOLD = 400;
 
+const DefaultInteractiveHeaderContent = () => (<i className="fas fa-arrow-down interactive-panel-indicator" />);
+const DefaultInteractiveFooterContent = () => (<i className="fas fa-arrow-up interactive-panel-indicator" />);
+const DefaultInteractiveLeftPaneContent = () => (<i className="fas fa-arrow-right interactive-panel-indicator" />);
+const DefaultInteractiveRightPaneContent = () => (<i className="fas fa-arrow-left interactive-panel-indicator" />);
+
 const SwipeView = ({
-    children, className, onSwipeFromTop, onSwipeFromBottom, onSwipeFromLeft, onSwipeFromRight,
+    children, className, fullScreen, onSwipeFromTop, onSwipeFromBottom, onSwipeFromLeft, onSwipeFromRight,
+    interactiveHeader, interactiveFooter, interactiveLeftPane, interactiveRightPane,
 }) => {
     const [isClicking, setIsClicking] = useState(false);
     const [pointerCoordinates, setPointerCoordinates] = useState(DEFAULT_COORDINATES);
     const [clickStartCoordinates, setClickStartCoordinates] = useState(DEFAULT_COORDINATES);
+    const [showInteractiveHeader, setShowInteractiveHeader] = useState(false);
+    const [showInteractiveFooter, setShowInteractiveFooter] = useState(false);
+    const [showInteractiveLeftPane, setShowInteractiveLeftPane] = useState(false);
+    const [showInteractiveRightPane, setShowInteractiveRightPane] = useState(false);
+
+    const allowInteractiveHeader = onSwipeFromTop
+        && isClicking
+        && clickStartCoordinates.y <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
+        && pointerCoordinates.y > clickStartCoordinates.y;
+
+    const allowInteractiveFooter = onSwipeFromBottom
+        && isClicking
+        && (window.innerHeight - clickStartCoordinates.y) <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
+        && pointerCoordinates.y < clickStartCoordinates.y;
+
+    const allowInteractiveLeftPane = onSwipeFromLeft
+        && isClicking
+        && clickStartCoordinates.x <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
+        && pointerCoordinates.x > clickStartCoordinates.x;
+
+    const allowInteractiveRightPane = onSwipeFromRight
+        && isClicking
+        && (window.innerWidth - clickStartCoordinates.x) <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
+        && pointerCoordinates.x < clickStartCoordinates.x;
+
+    useEffect(() => {
+        setShowInteractiveHeader(allowInteractiveHeader && !(showInteractiveFooter || showInteractiveLeftPane || showInteractiveRightPane));
+        setShowInteractiveFooter(allowInteractiveFooter && !(showInteractiveHeader || showInteractiveLeftPane || showInteractiveRightPane));
+        setShowInteractiveLeftPane(allowInteractiveLeftPane && !(showInteractiveHeader || showInteractiveFooter || showInteractiveRightPane));
+        setShowInteractiveRightPane(allowInteractiveRightPane && !(showInteractiveHeader || showInteractiveFooter || showInteractiveLeftPane));
+    }, [allowInteractiveHeader, allowInteractiveFooter, allowInteractiveLeftPane, allowInteractiveRightPane]);
 
     const onMouseMove = useCallback((event) => {
         if (!isClicking) {
@@ -32,8 +69,20 @@ const SwipeView = ({
     const onClickEnd = useCallback(() => {
         setIsClicking(false);
 
-        if (onSwipeFromTop && pointerCoordinates.y - clickStartCoordinates.y >= PULL_TO_VIEW_INTERACTIVE_PIXELS_DELTA) {
+        if (showInteractiveHeader && pointerCoordinates.y - clickStartCoordinates.y >= PULL_TO_VIEW_INTERACTIVE_PIXELS_HEIGHT) {
             onSwipeFromTop();
+        }
+
+        if (showInteractiveFooter && clickStartCoordinates.y - pointerCoordinates.y >= PULL_TO_VIEW_INTERACTIVE_PIXELS_HEIGHT) {
+            onSwipeFromBottom();
+        }
+
+        if (showInteractiveLeftPane && pointerCoordinates.x - clickStartCoordinates.x >= PULL_TO_VIEW_INTERACTIVE_PIXELS_WIDTH) {
+            onSwipeFromLeft();
+        }
+
+        if (showInteractiveRightPane && clickStartCoordinates.x - pointerCoordinates.x >= PULL_TO_VIEW_INTERACTIVE_PIXELS_WIDTH) {
+            onSwipeFromRight();
         }
     }, [pointerCoordinates, clickStartCoordinates]);
 
@@ -47,65 +96,97 @@ const SwipeView = ({
         };
     }, [onClickStart, onClickEnd]);
 
-    let interactiveHeader;
-    if (isClicking
-        && clickStartCoordinates.y <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
-        && pointerCoordinates.y > clickStartCoordinates.y) {
-        const height = Math.min(pointerCoordinates.y - clickStartCoordinates.y, PULL_TO_VIEW_INTERACTIVE_PIXELS_DELTA);
+    let interactiveHeaderDiv;
+    if (showInteractiveHeader) {
+        const height = Math.min(pointerCoordinates.y - clickStartCoordinates.y, PULL_TO_VIEW_INTERACTIVE_PIXELS_HEIGHT);
         const style = { height };
-        interactiveHeader = (
-            <div className="interactive-header" style={style}>
-                Interactive Header!
+        interactiveHeaderDiv = (
+            <div className="interactive-panel interactive-header" style={style}>
+                {interactiveHeader}
             </div>
         );
     }
 
-    let interactiveFooter;
-    if (isClicking
-        && (window.innerHeight - clickStartCoordinates.y) <= CLICK_START_INTERACTIVE_PIXELS_THRESHOLD
-        && pointerCoordinates.y < clickStartCoordinates.y) {
-        const height = Math.min(clickStartCoordinates.y - pointerCoordinates.y, PULL_TO_VIEW_INTERACTIVE_PIXELS_DELTA);
+    let interactiveFooterDiv;
+    if (showInteractiveFooter) {
+        const height = Math.min(clickStartCoordinates.y - pointerCoordinates.y, PULL_TO_VIEW_INTERACTIVE_PIXELS_HEIGHT);
         const style = { height };
-        interactiveFooter = (
-            <div className="interactive-footer" style={style}>
-                Interactive Footer!
+        interactiveFooterDiv = (
+            <div className="interactive-panel interactive-footer" style={style}>
+                {interactiveFooter}
             </div>
         );
     }
 
-    const divClassName = classnames('SwipeView', className);
+    let interactiveLeftPaneDiv;
+    if (showInteractiveLeftPane) {
+        const width = Math.min(pointerCoordinates.x - clickStartCoordinates.x, PULL_TO_VIEW_INTERACTIVE_PIXELS_WIDTH);
+        const style = { width };
+        interactiveLeftPaneDiv = (
+            <div className="interactive-panel interactive-left-pane" style={style}>
+                {interactiveLeftPane}
+            </div>
+        );
+    }
+
+    let interactiveRightPaneDiv;
+    if (showInteractiveRightPane) {
+        const width = Math.min(clickStartCoordinates.x - pointerCoordinates.x, PULL_TO_VIEW_INTERACTIVE_PIXELS_WIDTH);
+        const style = { width };
+        interactiveRightPaneDiv = (
+            <div className="interactive-panel interactive-right-pane" style={style}>
+                {interactiveRightPane}
+            </div>
+        );
+    }
+
+    const divClassName = classnames('SwipeView', className, {
+        fullscreen: fullScreen,
+    });
     return (
         <div
             className={divClassName}
             onMouseMove={onMouseMove}
         >
-            {interactiveHeader}
+            {interactiveHeaderDiv}
             <div className="swipe-view-body">
+                {interactiveLeftPaneDiv}
                 <div className="swipe-view-body-content">
                     {children}
                 </div>
+                {interactiveRightPaneDiv}
             </div>
-            {interactiveFooter}
+            {interactiveFooterDiv}
         </div>
     );
 };
 
 SwipeView.propTypes = {
     children: PropTypes.node,
-    className: PropTypes.string,
     onSwipeFromTop: PropTypes.func,
     onSwipeFromBottom: PropTypes.func,
     onSwipeFromLeft: PropTypes.func,
     onSwipeFromRight: PropTypes.func,
+    interactiveHeader: PropTypes.node,
+    interactiveFooter: PropTypes.node,
+    interactiveLeftPane: PropTypes.node,
+    interactiveRightPane: PropTypes.node,
+    className: PropTypes.string,
+    fullScreen: PropTypes.bool,
 };
 
 SwipeView.defaultProps = {
     children: null,
-    className: null,
     onSwipeFromTop: null,
     onSwipeFromBottom: null,
     onSwipeFromLeft: null,
     onSwipeFromRight: null,
+    interactiveHeader: <DefaultInteractiveHeaderContent />,
+    interactiveFooter: <DefaultInteractiveFooterContent />,
+    interactiveLeftPane: <DefaultInteractiveLeftPaneContent />,
+    interactiveRightPane: <DefaultInteractiveRightPaneContent />,
+    className: null,
+    fullScreen: true,
 };
 
 export default SwipeView;
