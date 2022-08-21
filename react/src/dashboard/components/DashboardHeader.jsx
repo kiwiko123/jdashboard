64846d8b73483f10d0ce8aef0c2ba580a8194806
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2022-08-21 11:19:40 -0700

    use component state manager for dashboard header

--- react/src/dashboard/components/DashboardHeader.jsx
+++ react/src/dashboard/components/DashboardHeader.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useStateManager } from 'state/hooks';
+import ComponentStateManager from 'state/components/ComponentStateManager';
+import JdashboardHeaderStateManager from '../header/JdashboardHeaderStateManager';
 import IconButton from '../../common/components/IconButton';
 import DashboardMenuSlideOverPane from './DashboardMenuSlideOverPane';
 
 import './styles/DashboardHeader.css';
 
-const DashboardHeader = (props) => (
-    <div className="DashboardHeader">
-        <div className="content">
-            <DashboardMenuSlideOverPane
-                appId={props.appId}
-                expanded={props.isMenuSlideOverExpanded}
-                toggleExpand={props.toggleMenuSlideOver}
-                accountProps={props.userData}
-            />
-            <IconButton
-                fontAwesomeClassName="fas fa-bars"
-                variant="outline-light"
-                onClick={props.toggleMenuSlideOver}
-            />
-            <h1 className="color-white">
-                {props.title}
-            </h1>
-        </div>
-    </div>
-);
+const DashboardHeader = (props) => {
+    const headerStateManager = useStateManager(() => new JdashboardHeaderStateManager());
+    const menuSlideOverPaneProps = {
+        appId: props.appId,
+        expanded: props.isMenuSlideOverExpanded,
+        toggleExpand: props.toggleMenuSlideOver,
+        accountProps: props.userData,
+    };
+
+    return (
+       <div className="DashboardHeader">
+           <div className="content">
+               <ComponentStateManager
+                   stateManager={headerStateManager}
+                   component={DashboardMenuSlideOverPane}
+                   staticProps={menuSlideOverPaneProps}
+               />
+               <IconButton
+                   fontAwesomeClassName="fas fa-bars"
+                   variant="outline-light"
+                   onClick={props.toggleMenuSlideOver}
+               />
+               <h1 className="color-white">
+                   {props.title}
+               </h1>
+           </div>
+       </div>
+   );
+};
 
 DashboardHeader.propTypes = {
     appId: PropTypes.string.isRequired,
