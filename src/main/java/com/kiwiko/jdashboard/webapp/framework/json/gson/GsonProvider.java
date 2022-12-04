11d64ecc4de2aabf5a8cd9541c4be150eff8d908
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2022-12-03 17:47:34 -0800

    allow GsonProvider to serialize Instant for table record version recording

--- src/main/java/com/kiwiko/jdashboard/webapp/framework/json/gson/GsonProvider.java
+++ src/main/java/com/kiwiko/jdashboard/webapp/framework/json/gson/GsonProvider.java
@@ -2,9 +2,14 @@ package com.kiwiko.jdashboard.webapp.framework.json.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.kiwiko.jdashboard.webapp.framework.json.gson.adapters.InstantTextAdapter;
+
+import java.time.Instant;
 
 public class GsonProvider {
-    private static final Gson DEFAULT_GSON = new Gson();
+    private static final Gson DEFAULT_GSON = new GsonBuilder()
+            .registerTypeAdapter(Instant.class, new InstantTextAdapter())
+            .create();
 
     public GsonBuilder builder() {
         return new GsonBuilder();
