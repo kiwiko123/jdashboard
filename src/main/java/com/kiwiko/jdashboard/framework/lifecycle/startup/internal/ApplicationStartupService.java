cc00d4f55a7ebb716d2b50aac321f43cd7ffef48
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2023-04-02 16:50:47 -0700

    log startup hook time

--- src/main/java/com/kiwiko/jdashboard/framework/lifecycle/startup/internal/ApplicationStartupService.java
+++ src/main/java/com/kiwiko/jdashboard/framework/lifecycle/startup/internal/ApplicationStartupService.java
@@ -3,20 +3,24 @@ package com.kiwiko.jdashboard.framework.lifecycle.startup.internal;
 import com.kiwiko.jdashboard.framework.lifecycle.startup.api.interfaces.ApplicationStartupHookException;
 import com.kiwiko.jdashboard.framework.lifecycle.startup.api.interfaces.ApplicationStartupHook;
 import com.kiwiko.jdashboard.framework.lifecycle.startup.registry.ApplicationStartupChain;
-import com.kiwiko.jdashboard.library.monitoring.logging.api.interfaces.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 
 import javax.inject.Inject;
+import java.time.Duration;
+import java.time.Instant;
 
 public class ApplicationStartupService implements ApplicationListener<ApplicationStartedEvent> {
 
-    @Inject private Logger logger;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationStartupService.class);
+
     @Inject private ApplicationStartupChain applicationStartupChain;
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
-        logger.info("Starting up application");
+        LOGGER.info("Starting up application...");
         sandboxRunHooks();
     }
 
@@ -25,16 +29,29 @@ public class ApplicationStartupService implements ApplicationListener<Applicatio
     }
 
     private void sandboxRunHook(ApplicationStartupHook hook) {
-        logger.info("Running startup hook {}", hook.getClass().getName());
+        LOGGER.info("Running startup hook {}", hook.getClass().getName());
+        long startTimeMs = System.currentTimeMillis();
+        Long endTimeMs = null;
 
         try {
             hook.run();
+            endTimeMs = System.currentTimeMillis();
         } catch (ApplicationStartupHookException e) {
-            logger.error("Error running startup hook {}", hook.getClass(), e);
+            LOGGER.error("Error running startup hook {}", hook.getClass(), e);
         } catch (Exception e) {
-            logger.error("Unexpected error running startup hook {}", hook.getClass(), e);
+            LOGGER.error("Unexpected error running startup hook {}", hook.getClass(), e);
         }
 
-        logger.info("Completed startup hook {}", hook.getClass().getName());
+        if (endTimeMs == null) {
+            LOGGER.info("Completed startup hook {}", hook.getClass().getName());
+        } else {
+            Instant startTime = Instant.ofEpochMilli(startTimeMs);
+            Instant endTime = Instant.ofEpochMilli(endTimeMs);
+            Duration hookRunDuration = Duration.between(startTime, endTime);
+            LOGGER.info(
+                    "Completed startup hook {} in {} milliseconds",
+                    hook.getClass().getName(),
+                    hookRunDuration.toMillis());
+        }
     }
 }
