eb30bcafdb13d15de15b44f7c7c941ca3f597236
kiwiko123 <kiwiko123@kiwiko123s-air.lan>
2023-08-24 22:05:54 -0700

    delete custom jdashboard property file reader

--- src/main/java/com/kiwiko/jdashboard/webapp/framework/application/properties/api/interfaces/JdashboardPropertyConstants.java
+++
@@ -1,5 +0,0 @@
-package com.kiwiko.jdashboard.webapp.framework.application.properties.api.interfaces;
-
-public class JdashboardPropertyConstants {
-    public static final String APPLICATION_LOGGING_ALLOWED_LEVELS = "app.logging.allowed_levels";
-}
--- src/main/java/com/kiwiko/jdashboard/webapp/framework/application/properties/internal/JdashboardPropertyFileConstants.java
+++
@@ -1,5 +0,0 @@
-package com.kiwiko.jdashboard.webapp.framework.application.properties.internal;
-
-class JdashboardPropertyFileConstants {
-    public static final String DEV_PROPERTIES_FILE_NAME = "jdashboard-config-dev.properties";
-}
